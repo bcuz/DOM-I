b030e35fc7096c88839be65a3326323debc27185
adam <bcuz@users.noreply.github.com>
2019-01-29 22:59:13 -0500

    getting started

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navLinks = document.querySelectorAll('nav a');
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].textContent = siteContent.nav[`nav-item-${i+1}`] 
+}
