cbd918d14571ef6537099ad911e53c3f6ee6feaf
adam <bcuz@users.noreply.github.com>
2019-01-30 23:13:11 -0500

    mvp complete.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,28 +1,38 @@
 let total = 0
 
-setInterval(function(){ 
-  if (total > 10000) {
-    document.querySelectorAll('.digits')
-    .forEach(link=> link.style.color = "red" )
-  }
+function begin() {
 
-  if (total % 10000 === 0) {
-    document.getElementById('secondTens').textContent = (total / 10000)
-  }
+let timer = setInterval(function(){ 
+    if (total > 10000) {
+      document.querySelectorAll('.digits')
+      .forEach(link=> link.style.color = "red" )
+      clearInterval(timer)
+    } else {
+      if (total % 10000 === 0) {
+        document.getElementById('secondTens').textContent = (total / 10000)
+      }
+    
+      if (total % 1000 === 0) {
+        document.getElementById('secondOnes').textContent = ((total / 1000) % 10)
+      }
+    
+      if (total % 100 === 0) {
+        document.getElementById('msHundreds').textContent = ((total / 100) % 10)
+      }
+    
+      if (total % 10 === 0) {
+        document.getElementById('msTens').textContent = ((total / 10) % 10)
+      }
+    
+      total += 10
+    }
+    
+  }, 10);
 
-  if (total % 1000 === 0) {
-    document.getElementById('secondOnes').textContent = ((total / 1000) % 10)
-  }
+}  
 
-  if (total % 100 === 0) {
-    document.getElementById('msHundreds').textContent = ((total / 100) % 10)
-  }
+let btn = document.querySelector("button");
 
-  if (total % 10 === 0) {
-    document.getElementById('msTens').textContent = ((total / 10) % 10)
-  }
-
-  total += 10
-
-}, 10);
-
+btn.addEventListener('click', function() {
+  begin()
+})
